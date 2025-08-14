70b525a75fa79f5c948236e2e3390b95986748ae
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2025-08-14 15:15:53 +0530

    add insert

--- ads/p1.c
+++ ads/p1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+void insert(int arr[], int dat, int pos){
+    arr[pos]=dat;
+}
+
 void main(){
     
     printf("Array Program\n");
-    int n,i;
+    int n,i,dat,pos;
     printf("Enter the number of elements: \n");
     scanf("%d",&n);
     int arr[n];
@@ -12,6 +16,15 @@ void main(){
         scanf("%d",&arr[i]);
     }
     printf("The elements in the array are ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\nNow Let's insert an element\n");
+    printf("Enter the element to insert: ");
+    scanf("%d",&dat);
+    printf("Enter the position to insert: ");
+    scanf("%d",&pos);
+    insert(arr,dat,pos);
+    printf("The new elements in the array are ");
     for (i = 0; i < n; i++)
         printf("%d ", arr[i]);
 }
